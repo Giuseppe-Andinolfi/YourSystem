d3acfa03ad700fbe2f4c62bcb7c85a54cc1daa1f
Giuseppe-Andinolfi <122208915+Giuseppe-Andinolfi@users.noreply.github.com>
2023-03-27 08:33:03 +0200

    Update CourseController.java

--- src/main/java/com/yourSystem/project/CourseController.java
+++ src/main/java/com/yourSystem/project/CourseController.java
@@ -1,14 +1,16 @@
 package com.yourSystem.project;
 import java.io.File;
-import java.io.FileInputStream;
+
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.List;
+import java.nio.file.Files;
 
-import org.apache.commons.io.IOUtils;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.springframework.core.io.ByteArrayResource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,7 +18,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
 
 @RestController
 @RequestMapping("/api/courses")
@@ -29,54 +34,56 @@ public class CourseController {
     }
 
     @PostMapping
-    public ResponseEntity<Void> createCourse(@RequestBody Course course) {
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createCourse(@RequestBody Course course) {
         courseService.createCourse(course);
-        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Course> getCourseById(@PathVariable Long id) {
+    public Course getCourseById(@PathVariable Long id) {
         Course course = courseService.getCourseById(id);
         if (course == null) {
-            return ResponseEntity.notFound().build();
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
         }
-        return ResponseEntity.ok(course);
+        return course;
     }
 
     @GetMapping
-    public ResponseEntity<List<Course>> getAllCourses() {
+    public List<Course> getAllCourses() {
         List<Course> courses = courseService.getAllCourses();
-        return ResponseEntity.ok(courses);
+        return courses;
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Void> updateCourse(@PathVariable Long id, @RequestBody Course course) {
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void updateCourse(@PathVariable Long id, @RequestBody Course course) {
         courseService.updateCourse(id, course);
-        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
-
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteCourse(@PathVariable Long id) {
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteCourse(@PathVariable Long id) {
         courseService.deleteCourse(id);
-        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 
+    public Workbook exportToExcel(String filePath) throws IOException {
+        Workbook workbook = courseService.exportToExcel();
+        FileOutputStream outputStream = new FileOutputStream(filePath);
+        workbook.write(outputStream);
+        outputStream.close();
+        return workbook;
+    }
     @GetMapping("/export/excel")
-    public ResponseEntity<byte[]> exportToExcel() throws IOException {
-        courseService.exportToExcel();
-
-        // Read the generated file and return its bytes as a response
-        File file = new File("courses.xlsx");
-        FileInputStream fis = new FileInputStream(file);
-        byte[] bytes = IOUtils.toByteArray(fis);
-        fis.close();
-
+    @ResponseStatus(HttpStatus.OK)
+    public ByteArrayResource exportToExcel() throws IOException {
+        String filePath = "courses.xlsx";
+        Workbook workbook = exportToExcel(filePath);
+        File file = new File(filePath);
+        byte[] fileContent = Files.readAllBytes(file.toPath());
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.parseMediaType("application/vnd.ms-excel"));
         headers.setContentDispositionFormData("attachment", "courses.xlsx");
         headers.setCacheControl("must-revalidate, post-check=0, pre-check=0");
-
-        return new ResponseEntity<>(bytes, headers, HttpStatus.OK);
+        return new ByteArrayResource(fileContent);
     }
 }
